a7b23371b10d33b4c430120bcac0f2e45c2ca418
AniAniGoud <138886448+AniAniGoud@users.noreply.github.com>
2023-07-11 15:27:59 +0530

    commit2
    attendance

--- project/src/main/java/ani/local.java
+++ project/src/main/java/ani/local.java
@@ -1,10 +1,21 @@
 package ani;
-
+import java.util.*;
 public class local {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("anila");
+		Scanner sc = new Scanner (System.in);
+		System.out.println("please enter held clases");
+		int held = sc.nextInt();
+		System.out.println("please enter no.of clases attend");
+		int attend = sc.nextInt();
+		int total = held /attend*100;
+		if(total>75) {
+			System.out.println("Allowed the clases");
+			
+		}
+		else {
+			System.out.println("Not allowed to the class");
+		}
 	}
 
 }
