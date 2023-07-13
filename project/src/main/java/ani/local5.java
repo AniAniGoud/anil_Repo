5f07b6c65bf76eaebb7b0f035a134f39745e201a
AniAniGoud <138886448+AniAniGoud@users.noreply.github.com>
2023-07-13 11:19:13 +0530

    commite
    codejava

---
+++ project/src/main/java/ani/local5.java
@@ -0,0 +1,26 @@
+package ani;
+import java.util.*;
+public class local5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("please enter the array size");
+        int size = sc.nextInt();
+        System.out.println("please enter the array elements");
+        int arr [] = new int [size];
+        for(int i=0; i<arr.length; i++) {
+        	arr[i]=sc.nextInt();
+        }
+         int   count=0;
+         for (int i=0; i<arr.length-1; i++) {
+        	 
+         if( arr[i]==arr[i+1]) {
+        	 
+        	 count++;
+        	 
+         }
+         System.out.print( count);
+       	}
+         }
+        		}
+               
