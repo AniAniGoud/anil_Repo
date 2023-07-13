b460866ea9e0b134be620474bee3fdfa9d9b2132
AniAniGoud <138886448+AniAniGoud@users.noreply.github.com>
2023-07-13 14:22:44 +0530

    commit

--- project/src/main/java/git/Demo1.java
+++ project/src/main/java/git/Demo1.java
@@ -4,9 +4,9 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=10;
-int b=20;
-int c=a+b;
+int x1=10;
+int y1=20;
+int c=x1+y1;
 System.out.println(c);
 	}
 
